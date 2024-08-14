e7fe22f35bc808ca0b707f7878707576f5bf7796
Vini <134401761+vini-destruidor19@users.noreply.github.com>
2024-08-14 07:51:53 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,63 @@
+const caixaPrincial = document.querySelector('.caixa-principal');
+const caixaPerguntas = document.querySelector('.caixa-perguntas');
+const caixaAlternativa = document.querySelector('.caixa-alternativa');
+const caixaResultado = document.querySelector('.caixa-resultado');
+const textoResultado = document.querySelector('texto-resultado');
+
+const perguntas = [
+    {
+        enunciado: "texto01",
+        alternativas: [
+            "alternativa01",
+            "alternativa02"
+        ]
+    },
+    {
+        enunciado: "texto02",
+        alternativas: [
+            "alternativa01",
+            "alternativa02"
+        ]
+    },
+    {
+        enunciado: "texto03",
+        alternativas: [
+            "alternativa01",
+            "alternativa02"
+        ]
+    },
+    {
+        enunciado: "texto04",
+        alternativas: [
+            "alternativa01",
+            "alternativa02"
+        ]
+    },
+    {
+        enunciado: "texto05",
+        alternativas: [
+            "alternativa01",
+            "alternativa02"
+        ]
+    }
+]
+
+let atual = 0;
+let perguntaAtual;
+
+function mostraPergunta(){
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+    mostraPergunta();
+}
+
+function mostrAltenativas(){
+    for( const alternativa of perguntaAtual.alternativa){
+        const BotaoAltenativas = document.createElement("button");
+        BotaoAltenativas.textContent =alternativa;
+        caixaAlternativas.appendChild(BotaoAltenativas)
+    }
+}
+ mostraPergunta();
+
 
